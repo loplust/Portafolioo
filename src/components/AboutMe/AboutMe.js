f9fc0be7c7fc9d4098aa9feb33bd6c049266ad72
Luis Alejandro Lopez <loplust3@gmail.com>
2022-11-10 14:56:27 -0500

    feat about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,22 +1,18 @@
-import React from 'react'
-import '../AboutMe/AboutMe.scss'
-export default function AboutMe ({ darkMode }) {
+import React from "react";
+import "../AboutMe/AboutMe.scss";
+export default function AboutMe({ darkMode }) {
   return (
-      <div className={`ContainerAboutme ${darkMode ? 'darkMode' : ''}`}>
-          <div className={`AboutMe ${darkMode ? 'darkMode' : ''}`}>
-        Sobre mi
-         </div>
-        <div className={`Me ${darkMode ? 'darkMode' : ''}`}>
-        Soy front-end developer.
-Actualmente, estudio desarrollo web de manera remota.
-Me encanta CSS, JavaScript y React.js.
-Estoy interesado en toda la parte visual de la web, desde el diseño a su desarrollo así como la usabilidad, adaptabilidad y la accesibilidad. Mi objetivo es crear experiencias visuales impresionantes y efectivas, cada sección al píxel.
-Me gusta desarrollar, hacer amistades, el contenido audiovisual, ver peliculas y escribir.
-Realice un Bootcamp en el cual aprendí mas sobre este mundo de la tecnología. 
-
-        </div>
-   
+    <div className={`ContainerAboutme ${darkMode ? "darkMode" : ""}`}>
+      <div className={`AboutMe ${darkMode ? "darkMode" : ""}`}>Sobre mi</div>
+      <div className={`Me ${darkMode ? "darkMode" : ""}`}>
+        Hola soy Front End Developer curioso y autodidacta, egresado del
+        bootcamp <a href="https://drive.google.com/file/d/1n9U0xxdDTFwAx0BiSM_NY86O2vWggNFC/view?usp=sharing" rel="noreferrer" target='_blank'>DEV.F</a> donde di con personas geniales que impactaron
+        positivamente mi aprendizaje e hicieron más fácil mis inicios. Tengo mas
+        de un año de experiencia en el cual he ido puliendo mis habilidades
+        técnicas desarrollando proyectos personales, velos <a href="/proyects">aquí</a> e incrementando mis ganas de seguir aprendiendo y creciendo
+        profesionalmente. Me encantan las películas, los videos
+        juegos y explorar el mundo.
       </div>
-    
-  )
+    </div>
+  );
 }
